44c8e8f6575092d225c825576390f346dc7d9016
Valeriy <taurenvd@gmail.com>
2016-09-09 14:38:32 +0300

    safe/load
    todo:optimize safe/load

---
+++ Assets/Resources/Scripts/Effects/Haste.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Haste : MonoBehaviour {
+
+   public GameObject part;
+
+   public float cooldown;
+   public float duration;
+   public float speedMult=5;
+
+	void Start ()
+    {
+        StartCoroutine(CorName(part, speedMult));
+    }
+    IEnumerator CorName(GameObject part,float speedMul)
+    {
+        while (GetComponent<dest>().death==false)
+        {
+            yield return new WaitForSeconds(3f);
+
+            GetComponent<NavMeshAgent>().speed *= speedMul;
+            gameObject.GetComponent<dest>().effects+= "Haste ";
+            if (gameObject.GetComponent<Animator>()!=null)
+            {
+             
+               gameObject.GetComponent<Animator>().SetBool("Run", true);
+                
+            }
+            part.SetActive(true);
+           
+                yield return new WaitForSeconds(6f);
+            GetComponent<NavMeshAgent>().speed /= speedMul;
+            part.SetActive(false);
+            if (gameObject.GetComponent<Animator>() != null)
+            {
+                gameObject.GetComponent<Animator>().SetBool("Run", false);
+                gameObject.GetComponent<dest>().effects = gameObject.GetComponent<dest>().effects.Replace("Haste", "");
+            }
+
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/bullet.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class bullet : MonoBehaviour
+{
+    public Transform target;
+
+
+    float step;
+    public float speed = 100;
+ 
+    public int radius;
+
+    void Start()
+    {
+    }
+    void Update()
+    {
+        if (target == null)
+        {
+
+            Destroy(gameObject);
+            return;
+        }
+        step = speed * Time.deltaTime;
+      
+
+        gameObject.transform.LookAt(target.transform);
+        gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, target.transform.position, step);
+
+
+
+
+    }
+    void Hit()
+    {
+     
+
+        if (radius == 0)
+        {
+            target.GetComponent<dest>().hp-=gameObject.GetComponentInParent<fire>().damage;
+        }
+        else
+        {
+            Collider[] cols = Physics.OverlapSphere(transform.position, radius);
+
+            foreach (Collider c in cols)
+            {
+                dest e = c.GetComponent<dest>();
+                if (e != null)
+                {
+                    e.GetComponent<dest>().hp -= gameObject.GetComponentInParent<fire>().damage;
+                }
+            }
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="Mobs"|| other.gameObject.tag == "Boss")
+        {
+            Hit();
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<TrailRenderer>().enabled = false;
+         // Destroy(gameObject);
+        }
+    }
+}
+
+
+
